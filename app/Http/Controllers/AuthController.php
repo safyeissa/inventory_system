29fca3f1af2d25821c4a798da269225d9779044a
safy eissa <safyeissa208@gmail.com>
2025-08-02 03:33:55 +0300

    Add Migrations , Models , Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+
+
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return response()->json([
+            'token' => $user->createToken('api-token')->plainTextToken,
+            'user' => $user
+        ]);
+    }
+
+
+
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+}
---
+++ database/migrations/2025_08_01_225155_create_stock_transfers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('inventory_item_id')->constrained()->onDelete('cascade');
+            $table->foreignId('from_warehouse_id')->constrained('warehouses')->onDelete('cascade');
+            $table->foreignId('to_warehouse_id')->constrained('warehouses')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_transfers');
+    }
+};
